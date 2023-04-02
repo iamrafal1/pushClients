e7a7b301ded844b5addf3d9044c7047431e17015
iamrafal1 <84661535+iamrafal1@users.noreply.github.com>
2023-04-02 17:57:31 +0100

    Added new example

--- server.go
+++ server.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/example1", handlerWrapper("templates/example1.html"))
+	http.HandleFunc("/example2", handlerWrapper("templates/example2.html"))
 	log.Fatal(http.ListenAndServe(":7070", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handlerWrapper(htmlFile string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	// Read in the template with SSE JavaScript code.
-	t, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		log.Fatal("error parsing template.")
+		// Read in the template with SSE JavaScript code.
+		t, err := template.ParseFiles(htmlFile)
+		if err != nil {
+			log.Fatal("error parsing template.")
 
+		}
+
+		// Render the template, writing to `w`.
+		t.Execute(w, nil)
+
+		// Done.
+		log.Println("Finished HTTP request at", r.URL.Path)
 	}
-
-	// Render the template, writing to `w`.
-	t.Execute(w, nil)
-
-	// Done.
-	log.Println("Finished HTTP request at", r.URL.Path)
 }
